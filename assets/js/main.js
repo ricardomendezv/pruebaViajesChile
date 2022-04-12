4674258011745a3b441e8267388db8185bf51dc1
ricardomendezv <rlmendez@uc.cl>
2022-04-12 05:08:21 -0400

    mejoras versión mobile

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    /*Se habilitan tooltips en toda la página*/
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+    
+})
